cfb1f335749684fe9345490133b09683b84c158a
nadeem <na.abbas001@gmail.com>
2020-09-21 13:13:14 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//feature 1
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">slik shopping cart</a>
+      </header>
+      <main>
+        product list
+      </main>
+      <footer>
+        All right
+      </footer>
+      
+      </div>
+  );
+}
+
+export default App;
